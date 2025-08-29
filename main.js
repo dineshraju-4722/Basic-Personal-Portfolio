221ff6b19c6e43ad008200ddb0125b1e0f6df41c
Raju <dineshsandireddy08@gmail.com>
2025-08-29 11:48:45 +0530

    second

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 let nav_lis = document.querySelectorAll('nav ul li');
-let lis_pages=document.querySelectorAll('.lis');
+let navbar = document.querySelector('nav ul');
+let lis_pages = document.querySelectorAll('.lis');
+let menubar = document.querySelector('.menu-bar');
+let closeicon = document.querySelector('.close-icon');
 
-let prev=0;
+let prev = 0;
 nav_lis[0].classList.add('li-border');
-lis_pages[5].style.display='flex';
+lis_pages[5].style.display = 'flex';
 for (let i = 0; i < nav_lis.length; i++) {
     nav_lis[i].addEventListener('click', () => {
         nav_lis[prev].classList.remove('li-border');
         nav_lis[i].classList.add('li-border');
-        lis_pages[prev].style.display='none';
-        lis_pages[i].style.display='flex';
-        prev=i;
+        lis_pages[prev].style.display = 'none';
+        lis_pages[i].style.display = 'flex';
+        navbar.style.display='';
+        prev = i;
     })
 }
+
+menubar.addEventListener('click', () => {
+    navbar.style.display = 'flex';
+})
+closeicon.addEventListener('click', () => {
+    navbar.style.display = '';
+})
+
+
 
 
